d2b6c2a1d02be4f5e542540699e2885cdf926102
Gobriel <gabriel.p.mckay@gmail.com>
2025-02-27 18:27:13 -0800

    pratice stuff

--- Practice/main.cpp
+++ Practice/main.cpp
@@ -2,23 +2,33 @@
 #include <string>
 #include <algorithm>
 #include <list>
+#include <set>
 
-using namespace std;
+int findMedian(std::multiset<int> s);
 
-int addOne(int num);
-bool compare(int num1, int num2);
-
+template <typename T>
+struct greater {
+    bool operator()(const T &a, const T &b) const {
+        return (a > b);
+    }
+};
 
 int main() {
-    list<int> l = {3, 6, 4, 1, 2, 8, 10};
-    list<int> lOne = {0, 0, 0, 0, 0, 0, 0, 0, 0};
-    transform(l.begin(), l.end(), lOne.begin(), addOne);
-    for (auto item: lOne) {
-        cout << item;
+    std::multiset<int, greater<int>> mySet {5, 5, 5, 5, 7, 1, 9, 10, 11, 67, 2, 5};
+    for (const auto &i: mySet) {
+        std::cout << i << ' ';
     }
 
 }
 
-int addOne(int num) {return num + 1;}
-
-bool compare(int num1, int num2) {return num1 < num2;}
+int findMedian(std::multiset<int> s) {
+    int middlePosition = s.size() / 2;
+    int count = 0;
+    for (auto i: s) {
+        count++;
+        if (count == middlePosition) {
+            return i;
+        }
+    }
+    return 0;
+}
