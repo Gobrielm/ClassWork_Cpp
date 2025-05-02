e36c7b0a3564bcfa135f1711d5b1a688d791565a
Gobriel <gabriel.p.mckay@gmail.com>
2025-05-01 18:31:30 -0700

    practice

--- Practice/main.cpp
+++ Practice/main.cpp
@@ -3,6 +3,8 @@
 #include <algorithm>
 #include <list>
 #include <set>
+#include <chrono>
+#include <thread>
 #include "animal.h"
 
 class Dog: private animal {
@@ -29,13 +31,26 @@ struct greater {
 
 
 int main() {
-    Dog* newDog = new Dog();
-    newDog->setName("A");
 
-    std::multiset<int, greater<int>> mySet {5, 5, 5, 5, 7, 1, 9, 10, 11, 67, 2, 5};
-    for (const auto &i: mySet) {
-        std::cout << i << ' ';
-    }
+    // std::chrono::steady_clock::time_point t1 = std::chrono::steady_clock::now();
+
+    // Dog* newDog = new Dog();
+    // newDog->setName("A");
+    //delete newDog;
+
+    // std::multiset<int, greater<int>> mySet {5, 5, 5, 5, 7, 1, 9, 10, 11, 67, 2, 5};
+    // for (const auto &i: mySet) {
+    //     std::cout << i << ' ';
+    // }
+    // for (int i = 0; i < 1000000000; i++) {
+    //     int x = 5 + 25;
+    // }
+
+    // std::chrono::steady_clock::time_point t2 = std::chrono::steady_clock::now();
+
+    // std::chrono::duration<double> time_spent = t2 - t1;
+    // std::cout << "\n";
+    // std::cout << time_spent.count() << "\n";
 
 }
 
