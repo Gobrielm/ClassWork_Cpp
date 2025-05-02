4d71d6a95d0eb86b721c586d5781a4e9f99777a1
Gobriel <gabriel.p.mckay@gmail.com>
2025-05-01 19:02:31 -0700

    prac

--- Practice/main.cpp
+++ Practice/main.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
+#include <vector>
 #include <string>
 #include <algorithm>
 #include <list>
 #include <set>
 #include <chrono>
 #include <thread>
+#include <mutex>
 #include "animal.h"
 
 class Dog: private animal {
@@ -18,7 +20,7 @@ class Dog: private animal {
 
 };
 
-
+void sum_v(int from, int to, std::vector<int> v, int* count);
 int findMedian(std::multiset<int> s);
 
 template <typename T>
@@ -29,11 +31,28 @@ struct greater {
 };
 
 
+std::mutex m;
 
 int main() {
+    std::vector<int> v;
+    int total = 10000000;
+    int* count = new int(0);
+    
+    for (int i = 0; i < total; i++) {
+        v.push_back(rand() % 20);
+    }
+    std::chrono::steady_clock::time_point t1 = std::chrono::steady_clock::now();
 
-    // std::chrono::steady_clock::time_point t1 = std::chrono::steady_clock::now();
+    std::thread th1(sum_v, 0, total / 4, v, count);
+    std::thread th2(sum_v, total / 4, total / 2, v, count);
+    std::thread th3(sum_v, total / 2, total / 4 * 3, v, count);
+    std::thread th4(sum_v, total / 4 * 3, total, v, count);
 
+    th1.join();
+    th2.join();
+    th3.join();
+    th4.join();
+    delete count;
     // Dog* newDog = new Dog();
     // newDog->setName("A");
     //delete newDog;
@@ -46,13 +65,26 @@ int main() {
     //     int x = 5 + 25;
     // }
 
-    // std::chrono::steady_clock::time_point t2 = std::chrono::steady_clock::now();
+    
 
-    // std::chrono::duration<double> time_spent = t2 - t1;
-    // std::cout << "\n";
-    // std::cout << time_spent.count() << "\n";
+
+    std::chrono::steady_clock::time_point t2 = std::chrono::steady_clock::now();
+    std::chrono::duration<double> time_spent = t2 - t1;
+    std::cout << "\n";
+    std::cout << time_spent.count() << "\n"; //0.066781
 
 }
+
+void sum_v(int from, int to, std::vector<int> v, int* count) {
+    int temp_count = 0;
+    for (int i = from; i < to; i++) {
+        temp_count += (v)[i];
+    }
+    m.lock();
+    *count = *count + temp_count;
+    m.unlock();
+}
+
 
 int findMedian(std::multiset<int> s) {
     int middlePosition = s.size() / 2;
