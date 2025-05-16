242e8452454730a1175ac0ac5efc53b12fe94cd9
Gobriel <gabriel.p.mckay@gmail.com>
2025-05-15 18:21:13 -0700

    understanding threads

--- Practice/main.cpp
+++ Practice/main.cpp
@@ -26,29 +26,46 @@ class Foo  {
 
 std::mutex m;
 
+void sum_v(int from, int to, const std::vector<int> v, int* count) {
+    int temp_count = 0;
+    
+    for (int i = from; i < to; i++) {
+        temp_count += (v)[i];
+    }
+    m.lock();
+    *count = *count + temp_count;
+    m.unlock();
+}
+
+void create_threads(int numThreads, const std::vector<int>& v, int* count) {
+    std::vector<std::thread*> threads;
+    for (int i = 0; i < numThreads; i++) {
+        m.lock();
+        std::thread* th = new std::thread(sum_v, v.size() / (numThreads) * (i), v.size() / (numThreads) * (i + 1), v, count);
+        m.unlock();
+        threads.push_back(th);
+    }
+
+    for (std::thread* x: threads) {
+        x -> join();
+    }
+}
+
 int main() {
 
 
-    // std::vector<int> v;
-    // int total = 10000000;
-    // int* count = new int(0);
+    std::vector<int> v;
+    int total = 200000000;
+    int* count = new int(0);
     
-    // for (int i = 0; i < total; i++) {
-    //     v.push_back(rand() % 20);
-    // }
-    // std::chrono::steady_clock::time_point t1 = std::chrono::steady_clock::now();
+    for (int i = 0; i < total; i++) {
+        v.push_back(1);
+    }
+    std::chrono::steady_clock::time_point t1 = std::chrono::steady_clock::now();
 
-    // std::thread th1(sum_v, 0, total / 4, v, count);
-    // std::thread th2(sum_v, total / 4, total / 2, v, count);
-    // std::thread th3(sum_v, total / 2, total / 4 * 3, v, count);
-    // std::thread th4(sum_v, total / 4 * 3, total, v, count);
-
-    // th1.join();
-    // th2.join();
-    // th3.join();
-    // th4.join();
-    // delete count;
-    // Dog* newDog = new Dog();
+    create_threads(2, v, count);
+    delete count;
+    // Dog* newDog = new Dog(); 1.3, 1.11, 17
     // newDog->setName("A");
     //delete newDog;
 
@@ -63,22 +80,14 @@ int main() {
     
 
 
-    // std::chrono::steady_clock::time_point t2 = std::chrono::steady_clock::now();
-    // std::chrono::duration<double> time_spent = t2 - t1;
-    // std::cout << "\n";
-    // std::cout << time_spent.count() << "\n"; //0.066781
+    std::chrono::steady_clock::time_point t2 = std::chrono::steady_clock::now();
+    std::chrono::duration<double> time_spent = t2 - t1;
+    std::cout << "\n";
+    std::cout << time_spent.count() << "\n"; //0.066781
 
 }
 
-void sum_v(int from, int to, std::vector<int> v, int* count) {
-    int temp_count = 0;
-    for (int i = from; i < to; i++) {
-        temp_count += (v)[i];
-    }
-    m.lock();
-    *count = *count + temp_count;
-    m.unlock();
-}
+
 
 
 int findMedian(std::multiset<int> s) {
